ecc45373e439df85f9ae66b0f181ef06ca4dde01
sylvain <sylvainmakak@gmail.com>
2020-12-16 10:17:46 -0500

    ajout de la fonction exo_02() imprimant bonjour

--- 02/programme.py
+++ 02/programme.py
@@ -1,15 +1,14 @@
 # -*- coding: utf-8 -*-
 """
-
 @author: CollegeBoreal
 """
 
 def exo_02():
-  b="bonjour"
-  return b
+  return ("Bonjour")
 
 def main():
   print( exo_02() )
 
 if __name__== "__main__":
   main()
+
