0c32d93835cb8c1e2d29930a6f8392727466d137
sylvain <sylvainmakak@gmail.com>
2020-12-16 09:59:41 -0500

    enlever le commentaire de la ligne 8

--- 01/programme.py
+++ 01/programme.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 """
 
-@author: CollegeBoreal
+@author: sylvainEmm
 """
 
 def main():
-  #print('Informatique: le rêve')
+  print('Informatique: le rêve')
 
 if __name__== "__main__":
   main()
