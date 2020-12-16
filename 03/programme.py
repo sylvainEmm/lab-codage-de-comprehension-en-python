af2191a1c38cf42d4781f00880198eab8b9c40e6
sylvain <sylvainmakak@gmail.com>
2020-12-16 11:05:14 -0500

    Ajout de la fonction exo_03() nombres pairs

--- 03/programme.py
+++ 03/programme.py
@@ -6,7 +6,7 @@
 
 def exo_03(liste):
   # Selectionner une liste de nombre pairs uniquement
-  pairs = [n*n for n in liste if n % 2== 0 ]
+  pairs = [n for n in liste if n % 2== 0 ]
   return sorted(pairs)
 
 def main():
